656c32fd9d5d1825190dc151a2433814f952a285
Mikhail617 <mikhail@rabweb.com>
2017-12-20 18:03:11 -0500

    Fix the website field. Remove some more extra characters from the brewery data fields .

--- ANDROID/BrewBuddy/app/src/main/java/com/example/mikhailefroimson/brewbuddy/MainActivity.java
+++ ANDROID/BrewBuddy/app/src/main/java/com/example/mikhailefroimson/brewbuddy/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 
 import java.io.InputStream;
 import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,13 +32,18 @@ public class MainActivity extends AppCompatActivity {
         Utils.CSVFile csvFile = new Utils.CSVFile(inputStream);
         List breweryList = csvFile.read();
 
+        // For Testing
         dataHelper.resetDatabase();
 
         for(Object breweryData:breweryList ) {
-            String[] brewery_data_for_db = {"", "", "", "", "", "", "", ""};
+            String[] brewery_data_for_db = {"", "", "", "", "", "", "", "", ""};
             for(int i = 0; i < (Array.getLength(((String []) breweryData))-1); i++) {
                 brewery_data_for_db[i] =  ((String []) breweryData)[i];
             }
+            //Log.d("DEBUG", "breweryData: " + Arrays.toString(((String []) breweryData)));
+            if (Array.getLength((String []) breweryData) >= 7)
+                //Log.d("DEBUG", ((String []) breweryData)[6]);
+                brewery_data_for_db[6] = ((String []) breweryData)[6];
             /*String name = ((String []) breweryData)[0];
             String address = ((String []) breweryData)[1] + " " + ((String []) breweryData)[2];
             String phone = ((String []) breweryData)[4];
@@ -46,24 +52,15 @@ public class MainActivity extends AppCompatActivity {
             Log.d("DEBUG", "" + ((String[]) breweryData)[0] + ", " + ((String[]) breweryData)[1] +
                               "," + ((String[]) breweryData)[2] + "," + ((String[]) breweryData)[3] +
                                 "," + ((String[]) breweryData)[4] + "," + ((String[]) breweryData)[5] +
-                                "," + ((String[]) breweryData)[6]);
-            //dataHelper.addBrewery(name, address, type, phone, website);*/
+                                "," + ((String[]) breweryData)[6]);*/
             String name = brewery_data_for_db[0].replace("\"", "");
             String address = brewery_data_for_db[1].replace("\"", "") + " " + brewery_data_for_db[2].replace("\"", "").replace("|", "");
             String phone = brewery_data_for_db[4].replace("Phone:", "");
-            String type = brewery_data_for_db[5];
+            String type = brewery_data_for_db[5].replace("Type:", "");
             String website = brewery_data_for_db[6];
-            Log.d("DEBUG", "name = " + name);
-            Log.d("DEBUG", "type = " + type);
-            Log.d("DEBUG", "address = " + address);
-            Log.d("DEBUG", "phone = " + phone);
-            Log.d("DEBUG", "website = " + website);
             if(address.contains("NJ"))
                 dataHelper.addBrewery(name, address, type, phone, website);
         }
-
-        // For Testing
-        //dataHelper.resetDatabase();
     }
 
     public void mapButtonClickFunction(View v)
